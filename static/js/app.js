05da7e3da35b5cb798b18c96ee231c371c07d567
Mayur Pande <mayurpande.uk@gmail.com>
2017-09-29 12:58:33 +0100

    wrote code to set html of .mobile-caption equal to whatever img is being displayed by backstretch

--- static/js/app.js
+++ static/js/app.js
@@ -10,7 +10,14 @@ $(document).ready(function(){
                 x.push('/static/img/' + data[i]['img_name']);
 
             }
+            var images =$.map(data,function(i) {return i.img_name; });
             $('#demo').backstretch(x,{duration:5000,fade:750});
+            $(window).on("backstretch.show", function(e, instance) {
+              var newCaption = data[instance.index].img_caption;
+              $(".mobile-caption").html( newCaption );
+            });
+
+
 
           }
 
