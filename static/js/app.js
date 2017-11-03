7142755b7d7f8b0993ee8962a86f7dd9a137c5e1
Mayur Pande <mayurpande.uk@gmail.com>
2017-11-03 15:43:27 +0000

    Changed #demo name to more convenient #background-slideshow

--- static/js/app.js
+++ static/js/app.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
             }
 
             var images =$.map(data,function(i) {return i.img_name; });
-            $('#demo').backstretch(x,{duration:5000,fade:750});
+            $('#background-slideshow').backstretch(x,{duration:5000,fade:750});
             $(window).on("backstretch.show", function(e, instance) {
               var newCaption = data[instance.index].img_caption;
               $(".caption").html( newCaption );
